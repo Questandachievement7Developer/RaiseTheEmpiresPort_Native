267d236b6b713765bfcc1da0b278b1e07100d5ca
Willy030125 <59504241+Willy030125@users.noreply.github.com>
2020-04-11 11:24:28 +0700

    Update custom ssh (Build 192)

--- empiresservercontainer/forwardHost.sh
+++ empiresservercontainer/forwardHost.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+rdm=$RANDOM
 
 echo '====================[Invitation Link]======================'
 echo 'Please connect through this'
-ssh -T -o "StrictHostKeyChecking=no" -o "ServerAliveInterval=60" -R 80:localhost:5005 ssh.localhost.run
+ssh -T -o "StrictHostKeyChecking=no" -o "ServerAliveInterval=60" -l "empires-$rdm" -R 80:localhost:5005 ssh.localhost.run
 if [ -f  killsigforwardHost ]; then
   rm killsigforwardHost
   exit
