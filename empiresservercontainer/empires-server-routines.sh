7669b1a6a1a9a8803f2f27dbe6f00e26eff84bf3
Willy030125 <59504241+Willy030125@users.noreply.github.com>
2020-04-12 16:11:58 +0700

    Update build 193

--- empiresservercontainer/empires-server-routines.sh
+++ empiresservercontainer/empires-server-routines.sh
@@ -1,17 +1,17 @@
-sudo apt-get install python3 python3-setuptools python3-dev git dialog ssh -y
-sudo pacman -S python git dialog python-setuptools openssh --noconfirm
-python3 -m pip install tendo py3amf flask flask_session flask_sqlalchemy flask_compress flask_socketio daiquiri git+git://github.com/christhechris/libscrc python-editor
+export origindir=$(pwd)
+export RUNTIMEDIR=${origindir}/._MEI202028
+pkg install python git clang dialog openssh -y
+python -m pip install tendo py3amf flask flask_session flask_sqlalchemy flask_compress flask_socketio daiquiri git+git://github.com/christhechris/libscrc python-editor
 export RaiseTheEmpiresRNTINI=RaiseTheEmpires/RaiseTheEmpires.ini
 echo "[Info]" > ${RaiseTheEmpiresRNTINI}
 echo "Name=RaiseTheEmpires_GNU_LINUX_EDITION" >> ${RaiseTheEmpiresRNTINI}
 echo "URL=https://github.com/AcidCaos/empires-and-allies" >> ${RaiseTheEmpiresRNTINI}
 echo "Binary=empires-server" >> ${RaiseTheEmpiresRNTINI}
 echo "[InstallFolders]" >> ${RaiseTheEmpiresRNTINI}
-echo "InstallPath=$(pwd)/RaiseTheEmpires" >> ${RaiseTheEmpiresRNTINI}
+echo "InstallPath=${RUNTIMEDIR}" >> ${RaiseTheEmpiresRNTINI}
 echo "MyGamesPath=$(pwd)/RaiseTheEmpires/fileSave" >> ${RaiseTheEmpiresRNTINI}
 echo "[InstallSettings]" >> ${RaiseTheEmpiresRNTINI}
 echo "Arch=$(uname -m)" >> ${RaiseTheEmpiresRNTINI}
-export TMPDIR=$(pwd)/RaiseTheEmpires/RNT
 export origindir=$(pwd)
 HEIGHT=15
 WIDTH=80
@@ -36,8 +36,8 @@ OPTIONS=(
 "changelog" "see the changelog"
 "RestoreSnapshot" "Restore Save File Snapshot"
 "ManualUpdate" "Triggers the empires-server to update manually if you had downloaded the newest version but no update"
-"MigratetoPC" "This will export the save file to your internal storage in RaiseTheEmpires folder"
-"MigratetoTermux" "This will import save file that is on RaiseTheEmpires Folder in your internal storage "
+"ExportSave" "This will export the save file to your internal storage in RaiseTheEmpires folder"
+"ImportSave" "This will import save file that is on RaiseTheEmpires Folder in your internal storage "
 "trim" "This will trim the storage usage but sacrifices the snapshot backup"
 "exit" "Exit from the empires-server boot menu")
 
